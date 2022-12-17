6448718569a7fdc0145ca8e6e9810edf929876de
Aristides da Costa <aristidiscosta200@gmail.com>
2022-12-17 19:24:26 +0100

    feat: adding data validation with yup

--- src/server/controller/cidades/Create.ts
+++ src/server/controller/cidades/Create.ts
@@ -1,12 +1,26 @@
 import { Request, Response } from "express";
+import * as yup from "yup";
 
 interface ICidade {
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+const yupValidation: yup.SchemaOf<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
 
-  console.log(req.body.nome);
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade | undefined = undefined;
+  try {
+    validatedData = await yupValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
 
-  return res.send("Create!");
+    return res.json({
+      errors: {
+        default: yupError.message,
+      },
+    });
+  }
+  console.log(validatedData);
 };
